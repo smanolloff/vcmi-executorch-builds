f8d9162c9612a95b66ebf0a5fdb8bad36c45f134
Simeon Manolov <s.manolloff@gmail.com>
2025-10-04 01:20:23 +0300

    fix scripts

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ BUILD_TYPE=${2:-Release}
 cd executorch
 
 case "$TARGET" in
-msvc-*)
+win-*)
     . .venv/Scripts/activate ;;
 *)
     . .venv/bin/activate ;;
@@ -18,79 +18,7 @@ esac
 flags=("-DCMAKE_BUILD_TYPE=$BUILD_TYPE")
 
 case "$TARGET" in
-android-32)
-    # https://github.com/pytorch/executorch/pull/12146
-    git -c core.autocrlf=false apply ../patch/android32.patch
-    flags+=(
-        -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
-        -DANDROID_ABI=armeabi-v7a
-        -DANDROID_ARM_NEON=ON
-        -DANDROID_PLATFORM=19
-    )
-    ;;
-android-64)
-    flags+=(
-        -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
-        -DANDROID_ABI=arm64-v8a
-        -DANDROID_PLATFORM=21     # os.api_level in VCMI android-64 conan profile
-    )
-    ;;
-android-64-intel)
-    flags+=(
-        -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
-        -DANDROID_ABI=x86_64
-        -DANDROID_PLATFORM=21     # os.api_level in VCMI android-x64 conan profile
-    )
-    ;;
-clang-latest-debug)
-    # No special flags
-    ;;
-clang-oldest-release)
-    # No special flags
-    ;;
-gcc-latest-release)
-    # No special flags
-    ;;
-gcc-oldest-debug)
-    # No special flags
-    ;;
-ios)
-    flags+=(
-      -DCMAKE_SYSTEM_NAME=iOS
-      -DCMAKE_OSX_SYSROOT=iphoneos
-      -DCMAKE_OSX_ARCHITECTURES=arm64
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0    # as per VCMI's root CMakeLists
-    )
-    ;;
-mac-arm)
-    flags+=(
-        -DCMAKE_OSX_ARCHITECTURES=arm64
-        -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0     # os.version in VCMI macos-arm conan profile
-    )
-    ;;
-mac-intel)
-    flags+=(
-        -DCMAKE_OSX_ARCHITECTURES=x86_64
-        -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13     # os.version in VCMI macos-intel conan profile
-    )
-    ;;
-msvc-arm64)
-    export PYTHON_EXECUTABLE=$VIRTUAL_ENV/Scripts/python.exe
-    git -c core.autocrlf=false apply ../patch/windows.patch
-    flags+=(
-        -G 'Visual Studio 17 2022'
-        -T ClangCL
-        -A ARM64
-        -DPYTHON_EXECUTABLE=$VIRTUAL_ENV/Scripts/python.exe
-        -DCMAKE_PREFIX_PATH=$VIRTUAL_ENV/Lib/site-packages
-        -DCMAKE_C_FLAGS="/clang:-march=armv8.2-a+fp16+dotprod+i8mm+bf16"
-        -DCMAKE_CXX_FLAGS="/EHsc /clang:-march=armv8.2-a+fp16+dotprod+i8mm+bf16"
-        -DEXECUTORCH_BUILD_KERNELS_OPTIMIZED=OFF
-        -DEXECUTORCH_BUILD_KERNELS_QUANTIZED=OFF
-        -DXNNPACK_ENABLE_ASSEMBLY=OFF
-    )
-    ;;
-msvc-x64)
+win-x64)
     export PYTHON_EXECUTABLE=$VIRTUAL_ENV/Scripts/python.exe
     git -c core.autocrlf=false apply ../patch/windows.patch
     flags+=(
@@ -116,7 +44,7 @@ msvc-x64)
         # -DXNNPACK_ENABLE_AVX512FP16=OFF
     )
     ;;
-msvc-x86)
+win-x86)
     export PYTHON_EXECUTABLE=$VIRTUAL_ENV/Scripts/python.exe
     git -c core.autocrlf=false apply ../patch/windows.patch
     git -c core.autocrlf=false apply --whitespace=nowarn ../patch/windows-x86.patch
@@ -130,6 +58,69 @@ msvc-x86)
         -DXNNPACK_ENABLE_AVX256VNNI=OFF
         -DXNNPACK_ENABLE_AVX256VNNIGFNI=OFF
         -DXNNPACK_ENABLE_AVX512BF16=OFF
+    )
+    ;;
+win-arm64)
+    export PYTHON_EXECUTABLE=$VIRTUAL_ENV/Scripts/python.exe
+    git -c core.autocrlf=false apply ../patch/windows.patch
+    flags+=(
+        -G 'Visual Studio 17 2022'
+        -T ClangCL
+        -A ARM64
+        -DPYTHON_EXECUTABLE=$VIRTUAL_ENV/Scripts/python.exe
+        -DCMAKE_PREFIX_PATH=$VIRTUAL_ENV/Lib/site-packages
+        -DCMAKE_C_FLAGS="/clang:-march=armv8.2-a+fp16+dotprod+i8mm+bf16"
+        -DCMAKE_CXX_FLAGS="/EHsc /clang:-march=armv8.2-a+fp16+dotprod+i8mm+bf16"
+        -DEXECUTORCH_BUILD_KERNELS_OPTIMIZED=OFF
+        -DEXECUTORCH_BUILD_KERNELS_QUANTIZED=OFF
+        -DXNNPACK_ENABLE_ASSEMBLY=OFF
+    )
+    ;;
+linux-x64)
+    # no special flags
+    ;;
+macos-x64)
+    flags+=(
+        -DCMAKE_OSX_ARCHITECTURES=x86_64
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13     # os.version in VCMI macos-intel conan profile
+    )
+    ;;
+macos-arm64)
+    flags+=(
+        -DCMAKE_OSX_ARCHITECTURES=arm64
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0     # os.version in VCMI macos-arm conan profile
+    )
+    ;;
+ios-arm64)
+    flags+=(
+      -DCMAKE_SYSTEM_NAME=iOS
+      -DCMAKE_OSX_SYSROOT=iphoneos
+      -DCMAKE_OSX_ARCHITECTURES=arm64
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0    # as per VCMI's root CMakeLists
+    )
+    ;;
+android-arm64)
+    flags+=(
+        -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
+        -DANDROID_ABI=arm64-v8a
+        -DANDROID_PLATFORM=21     # os.api_level in VCMI android-64 conan profile
+    )
+    ;;
+android-armv7)
+    # https://github.com/pytorch/executorch/pull/12146
+    git -c core.autocrlf=false apply ../patch/android-armv7.patch
+    flags+=(
+        -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
+        -DANDROID_ABI=armeabi-v7a
+        -DANDROID_ARM_NEON=ON
+        -DANDROID_PLATFORM=19
+    )
+    ;;
+android-x64)
+    flags+=(
+        -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
+        -DANDROID_ABI=x86_64
+        -DANDROID_PLATFORM=21     # os.api_level in VCMI android-x64 conan profile
     )
     ;;
 *)
